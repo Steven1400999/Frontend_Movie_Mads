0d02c9f40a38e3efb43f77b9f2317ad83c594dca
Steven <quezadasteven570@gmail.com>
2024-05-04 21:46:57 -0600

    antes de probar variable global

---
+++ components/Home/Movie_Description.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Box, Text, Image, Card } from '@gluestack-ui/themed';
+
+const Movie_Description = ({}) => {
+
+  return (
+    <Card size="md" variant="elevated" padding={0} margin={0} borderWidth={0} borderRadius="$2xl">
+      <Box position='relative'>
+        <Text color='black'>Hola, como estamos </Text>
+      </Box>
+    </Card>
+  );
+};
+
+export default Movie_Description;
